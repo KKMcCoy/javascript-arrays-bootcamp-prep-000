e3bd01c842e5b998feac79850176247350ff0544
karen tsui <buzz.kyt@gmail.com>
2018-07-16 14:38:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,19 @@
 var chocolateBars = ["snicker", "hundred grand", "kitkat", "skittles"]
 
-addElementToBeginningOfArray(array, element) {
+function addElementToBeginningOfArray(array, element) {
   return [element,...array]; 
 }
 
-destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array,element]; 
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
   return array;
 }
