8a59e6c20bc22913fb3fb56c9d01635762631f6c
karen tsui <buzz.kyt@gmail.com>
2018-07-16 14:43:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFroBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift;
   return array;
 }
